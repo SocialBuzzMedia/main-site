e881bafe7df61bd1c7c182f08bab3d8c7962e0cb
Social Buzz Media <hellosocialbuzzmedia@gmail.com>
2024-09-19 19:26:29 +0530

    Content Change of 404 Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,18 +1,18 @@
 const NotFound = () => {
     return (
         <div>
-            <div>
-                <h1 className="text-center text-6xl text-red-500">
-                    404 - Page Not Found
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-10/12">
+                <div className="block text-5xl text-red-500 mb-4 font-bold text-center">
+                    Oops.....
+                </div>
+                <h1 className="text-center text-6xl text-red-500 mb-10">
+                    Sometimes you need to get lost to find your Way
                 </h1>
-                <p className="text-center text-2xl">
-                    We&aosp;re sorry, but the page you&aosp;re looking for
-                    doesn&aosp;t exist.
-                </p>
+                <p className="text-center text-2xl">404 ERROR!</p>
                 <div className="flex justify-center mt-10">
                     <a
                         href="/"
-                        className="px-5 py-3 rounded-md text-white bg-red-500 hover:bg-red-600"
+                        className="px-5 py-3 rounded-full text-white bg-red-500 hover:bg-red-600 shadow-lg"
                     >
                         Go Back Home
                     </a>
